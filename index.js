eac4a12975f4d89681c784288b9aa6055e4ecfb5
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-03 06:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let num1 = 31
 let num2 = 2
 // Write your code here
-let multiply = num1 * num2
+let multiply = num1 * num2;
 
-let random = Math.floor(Math.random() * 10)
+let random = Math.floor(Math.random() * 10);
 
 // let mod = 24 % 5
